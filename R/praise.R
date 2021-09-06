31c29d73861e56c4450b6c87c87250065cfddaf2
Dean Milne <deano14m@gmail.com>
2021-09-07 09:49:29 +1000

    Initial Commit for praiseme package

--- R/praise.R
+++ R/praise.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param name
+#' @param punctuation
+#'
+#' @return "You're the best, {name}{punctuation}"
+#' @export
+#'
+#' @examples
 praise <- function(name, punctuation = "!"){
   glue::glue("You're the best, {name}{punctuation}")
 }
