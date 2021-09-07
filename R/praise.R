d79a5e1675a48c74a736e2e70468e8001f443af5
Dean Milne <deano14m@gmail.com>
2021-09-07 10:21:47 +1000

    Fixing Stuff up

--- R/praise.R
+++ R/praise.R
@@ -1,12 +1,15 @@
-#' Title
+#' Deliver praise
+#' @description This function is super useful when you're feeling sad
+#' and is useful for everyone.
 #'
-#' @param name
-#' @param punctuation
+#' @param name text string;  This is the name of the person I want to praise
+#' @param punctuation text string;  This is our emphasis as a text input.
 #'
-#' @return "You're the best, {name}{punctuation}"
+#' @return Text string with praise
 #' @export
 #'
 #' @examples
+#' praise(name = "Steph", punctuation = "!")
 praise <- function(name, punctuation = "!"){
   glue::glue("You're the best, {name}{punctuation}")
 }
